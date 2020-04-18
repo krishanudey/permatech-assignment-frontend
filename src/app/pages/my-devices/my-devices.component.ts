a65ee6674038d079da815451cc6034b2ba4fbc90
Krishanu Dey <krishanudey99@gmail.com>
2020-04-18 19:55:42 +0530

    System change backup

--- src/app/pages/my-devices/my-devices.component.ts
+++ src/app/pages/my-devices/my-devices.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
+import { ApiService } from "src/app/services/api.service";
+import { Subscription } from "rxjs";
+import { DeviceConfig } from "src/app/models/device-config";
 
 @Component({
   selector: "app-my-devices",
@@ -6,8 +9,17 @@ import { Component, OnInit, OnDestroy } from "@angular/core";
   styleUrls: ["./my-devices.component.scss"],
 })
 export class MyDevicesComponent implements OnInit, OnDestroy {
-  constructor() {}
+  myDevices: DeviceConfig[] = [];
+  private myDeviceSubscription: Subscription;
+  constructor(private api: ApiService) {}
 
-  ngOnInit() {}
-  ngOnDestroy() {}
+  ngOnInit() {
+    this.myDeviceSubscription = this.api.getMyDevices().subscribe((devices) => {
+      this.myDevices = devices;
+      console.log(this.myDevices);
+    });
+  }
+  ngOnDestroy() {
+    this.myDeviceSubscription.unsubscribe();
+  }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+import { NetworkDevice } from "../models/network-device";
+import { DeviceConfig } from "../models/device-config";
 
 @Injectable({
   providedIn: "root",
@@ -7,4 +11,14 @@ import { HttpClient } from "@angular/common/http";
 export class ApiService {
   apiRoot: string = `http://${window.location.hostname}:8080/api/v1`;
   constructor(private http: HttpClient) {}
+
+  discoverDevices(): Observable<NetworkDevice[]> {
+    let apiURL = `${this.apiRoot}/devices/discover`;
+    return this.http.get<NetworkDevice[]>(apiURL);
+  }
+
+  getMyDevices(): Observable<DeviceConfig[]> {
+    let apiURL = `${this.apiRoot}/devices`;
+    return this.http.get<DeviceConfig[]>(apiURL);
+  }
 }
