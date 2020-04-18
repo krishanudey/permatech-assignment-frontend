ee263f9040f0743d907ddbd2c65961a0df210066
Krishanu Dey <krishanudey99@gmail.com>
2020-04-19 00:43:16 +0530

    Adds add device method in api service

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -21,4 +21,24 @@ export class ApiService {
     let apiURL = `${this.apiRoot}/devices`;
     return this.http.get<DeviceConfig[]>(apiURL);
   }
+
+  addDevice(name: string, nd: NetworkDevice): Observable<DeviceConfig> {
+    let apiURL = `${this.apiRoot}/devices`;
+    return this.http.post<DeviceConfig>(apiURL, {
+      name: name,
+      uuid: nd.uuid,
+      ip: nd.ip,
+      servicePort: nd.servicePort,
+      type: nd.type,
+    });
+  }
 }
+
+export const HTTP_API_ERROR_CODE = {
+  VALIDATION_FAILED: "VALIDATION_FAILED",
+  DEVICE_ALREADY_ADDED: "DEVICE_ALREADY_ADDED",
+  DUPLICATE_DEVICE_NAME: "DUPLICATE_DEVICE_NAME",
+  INVALID_ACTION: "INVALID_ACTION",
+  NOT_FOUND: "NOT_FOUND",
+  INTERNAL_SERVER_ERROR: "INTERNAL_SERVER_ERROR",
+};
