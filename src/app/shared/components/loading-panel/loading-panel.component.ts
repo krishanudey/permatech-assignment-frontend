0844937d5ecb28db4bacf5c2d779b6601f94d71a
Krishanu Dey <krishanudey99@gmail.com>
2020-04-19 00:41:56 +0530

    Adds loading panel component, sweet alert 2 support

---
+++ src/app/shared/components/loading-panel/loading-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-panel',
+  templateUrl: './loading-panel.component.html',
+  styleUrls: ['./loading-panel.component.scss']
+})
+export class LoadingPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
